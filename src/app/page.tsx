41e7d632095e321c28363cfce70f239e331c8c24
reginaldoaevo-lgtm <reginaldo.aevo@gmail.com>
2026-03-30 16:00:23 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
 "use client";
+import App from "../App.tsx";
 
-import App from "../App";
-import { ErrorBoundary } from "../components/ErrorBoundary";
-
-export default function Home() {
-  return (
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  );
+export default function Page() {
+  return <App />;
 }
